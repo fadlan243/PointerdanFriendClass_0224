7c7161f5a0cd5362629e92621d020aeae96c9ee7
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2025-05-05 21:17:30 +0700

    friend class satu commit

--- friendClasssatu.cpp
+++ friendClasssatu.cpp
@@ -45,4 +45,55 @@ int main()
     joko_siswa.setId(1);
     joko_siswa.displayAll(joko);
     return 0;
-}    
+}
+
+class mahasiswa
+{
+private:
+    string nama;
+
+public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
+}
+
+class pelajar;
+class manusia 
+{
+public:
+    void showNilaiPelajar(pelajar &x);
+};
+
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
+
+void manusia::showNilaiPelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
+}
